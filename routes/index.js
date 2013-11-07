9daddf8c0547fd49ff1b307ba24501ad6ce37480
stefanRitter <stefan@stefanritter.com>
2013-11-07 06:51:25 +0000

    add name

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Homepage' });
+  res.render('index', { title: 'Trybes' });
 };
 
 exports.contact = function(req, res){
-  res.render('index', { title: 'Homepage' });
+  res.render('index', { title: 'Trybes' });
 };
 
 exports.terms = function(req, res){
-  res.render('index', { title: 'Homepage' });
+  res.render('index', { title: 'Trybes' });
 };
 
 exports.privacy = function(req, res){
-  res.render('index', { title: 'Homepage' });
+  res.render('index', { title: 'Trybes' });
 };
 
 exports.about = function(req, res){
-  res.render('index', { title: 'Homepage' });
+  res.render('index', { title: 'Trybes' });
 };
