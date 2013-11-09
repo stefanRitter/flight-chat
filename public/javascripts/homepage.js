d73f4f5c530f14fe14137f6fd1ce446b99271688
stefanRitter <stefan@stefanritter.com>
2013-11-09 16:41:25 +0000

    add bgs

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,5 +1,13 @@
 jQuery(function() {
   'use strict';
+  
+  // prefech backgrounds
+  var imgOutdoor = new Image();
+  imgOutdoor.src = '/images/outdoor.jpg';
 
+  // cicle background
+  //setInterval(function() {
+  //  $('html').toggleClass('background-outdoor');
+  //}, 3000);
 
 });
