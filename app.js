aeb5219de08debb64c8258f98da65895676aeeaf
stefanRitter <stefan@stefanritter.com>
2013-11-09 20:56:54 +0000

    start adding rough validation

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 
-/**
- * Module dependencies.
- */
-
+// dependencies
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -21,17 +18,20 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
 // routes
 app.get('/', routes.index);
 app.get('/about', routes.about);
-app.get('/claim', routes.claim);
 
 
+// server
 http.createServer(app).listen(app.get('port'), function(){
+  'use strict';
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,17 @@
  */
 
 exports.index = function(req, res){
+  'use strict';
   res.render('index', { title: 'Trybes' });
 };
 
 exports.about = function(req, res){
+  'use strict';
   res.render('about', { title: 'Trybes - About' });
 };
 
-exports.claim = function(req, res){
-  res.render('index', { title: 'Trybes - Claim Invite' });
-};
+
+function validateEmail(email) {
+  'use strict';
+  return email.match(/^[\S]+@[\S]+\.[\S]+$/);
+}
