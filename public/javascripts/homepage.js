640306fdee71d6ec73254ef0b928f5759ab209ba
stefanRitter <stefan@stefanritter.com>
2013-11-10 17:25:14 +0000

    add error checking

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -15,7 +15,18 @@ jQuery(function() {
   }, 3000);*/
 
   $('button').on('click touch', function(e) {
-    e.preventDefault();
-    $(e.target).html(imgSpinnerBlack).prev('input').css('opacity', '0.8');
+    var email = $('input[type=email]').val();
+    if (validateEmail(email)) {
+      $(e.target).html(imgSpinnerBlack).prev('input').css('opacity', '0.8');
+      $('.error').addClass('hidden');
+    } else {
+      e.preventDefault();
+      $('.error').removeClass('hidden');
+    }
   });
+
+  // helpers
+  function validateEmail(email) {
+    return email.match(/^[\S]+@[\S]+\.[\S]+$/);
+  }
 });
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@
 module.exports = exports = function(app, db) {
   'use strict';
 
+
+  // routes
   app.get('/', index);
+  app.post('/', addEmail);
+
   app.get('/about', about);
-  
+  app.post('/about', addEmail);
+
+
+  // request handlers
   function index(req, res){
     res.render('index', { title: 'TRYBES' });
   }
@@ -12,6 +19,30 @@ module.exports = exports = function(app, db) {
   function about(req, res){
     res.render('about', { title: 'TRYBES - About' });
   }
+
+  function addEmail(req, res) {
+    var emailAddress = req.body.email,
+        emails = null,
+        email = null;
+
+    if (!validateEmail(emailAddress)) {
+      return res.render('index', { title: 'TRYBES',
+                                   error: 'Sorry, your email address was invalid. Please correct any mistakes and try again.' });
+    }
+
+    // persist if valid
+    emails = db.collection('emails');
+    email = { '_id': emailAddress,
+              'date': new Date()};
+
+    emails.insert(email, function(err,doc) {
+      if (err) { return console.error(err); }
+      console.log('inserted: ' + JSON.stringify(doc));
+    });
+
+    res.render('index', {title: 'TRYBES', success: true});
+  }
+
 
   // helpers
   function validateEmail(email) {
