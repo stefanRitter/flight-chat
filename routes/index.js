484e7398901bb2d187b6e4846ab587626daaae31
stefanRitter <stefan@stefanritter.com>
2013-11-10 18:15:43 +0000

    persist ips

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ module.exports = exports = function(app, db) {
     emails = db.collection('emails');
     email = { '_id': emailAddress,
               'date': new Date(),
-              'ip': req.ip};
+              'ip': req.ips};
 
     emails.insert(email, function(err,doc) {
       if (err) { return console.error(err); }
