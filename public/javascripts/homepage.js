31949899d8b5060341d1ba946f7fc05cff78a032
stefanRitter <stefan@stefanritter.com>
2013-11-10 12:05:45 +0000

    add about page

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -2,12 +2,20 @@ jQuery(function() {
   'use strict';
   
   // prefech backgrounds
-  var imgOutdoor = new Image();
+  var imgOutdoor = new Image(),
+      imgSpinnerBlack = new Image();
+  
   imgOutdoor.src = '/images/outdoor.jpg';
+  imgSpinnerBlack.src = '/images/spinner-white.png';
+  imgSpinnerBlack.className = 'animRotateRound';
 
   // cicle background
   //setInterval(function() {
   //  $('html').toggleClass('background-outdoor');
   //}, 3000);
 
+  $('button').on('click touch', function(e) {
+    e.preventDefault();
+    $(e.target).html(imgSpinnerBlack).prev('input').css('opacity', '0.8');
+  });
 });
