3dc5d812008c3b0f0e0474ba8db58a8b4080e8aa
stefanRitter <stefan@stefanritter.com>
2013-11-10 16:07:40 +0000

    add mongodb

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 
 // dependencies
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-var app = express();
+var express = require('express'),
+    MongoClient = require('mongodb').MongoClient,
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    app = express(),
+    datastoreURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/test';
 
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-
-// routes
-app.get('/', routes.index);
-app.get('/about', routes.about);
-
-
-// server
-http.createServer(app).listen(app.get('port'), function(){
+// connect to database
+MongoClient.connect(datastoreURI, function(err, db) {
   'use strict';
-  console.log('Express server listening on port ' + app.get('port'));
+  if (err) { throw err; }
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // development only
+  if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  // routes
+  routes(app, db);
+
+  // server
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
+module.exports = exports = function(app, db) {
   'use strict';
-  res.render('index', { title: 'TRYBES' });
+
+  app.get('/', index);
+  app.get('/about', about);
+  
+  function index(req, res){
+    res.render('index', { title: 'TRYBES' });
+  }
+
+  function about(req, res){
+    res.render('about', { title: 'TRYBES - About' });
+  }
+
+  // helpers
+  function validateEmail(email) {
+    return email.match(/^[\S]+@[\S]+\.[\S]+$/);
+  }
 };
-
-exports.about = function(req, res){
-  'use strict';
-  res.render('about', { title: 'TRYBES - About' });
-};
-
-
-function validateEmail(email) {
-  'use strict';
-  return email.match(/^[\S]+@[\S]+\.[\S]+$/);
-}
