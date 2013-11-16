c7ca896560c7023e327c8657cb435c4d1619a4ad
stefanRitter <stefan@stefanritter.com>
2013-11-16 20:31:22 +0000

    add active indication

--- public/js/component_ui/switch_section.js
+++ public/js/component_ui/switch_section.js
@@ -9,14 +9,18 @@ define(function (require) {
   function switchSection () {
     // attributes
     this.$self = null;
+    this.$nav = null;
     
     // initialize
     this.after('initialize', function () {
       this.$self = $(this.$node.data('id'));
+      this.$nav = $('header.main-header li');
 
       this.on('click touch', function(e) {
         $('section').addClass('out');
         this.$self.removeClass('out');
+        this.$nav.removeClass('active');
+        this.$node.addClass('active');
       });
     });
   }
