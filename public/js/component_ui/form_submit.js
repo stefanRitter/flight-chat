de992a8f20fa9f4e437aef42fa49f003d4f7ef77
stefanRitter <stefan@stefanritter.com>
2013-11-23 14:04:59 +0000

    combine authentication pages css

--- public/js/component_ui/form_submit.js
+++ public/js/component_ui/form_submit.js
@@ -11,11 +11,27 @@ define(function (require) {
   return defineComponent(formSubmit);
 
   function formSubmit() {
+    this.siblings = [];
+    this.$form = {};
+    this.buttonText = '';
+
     // initialize
     this.after('initialize', function () {
+      this.siblings = this.$node.siblings('input');
+      this.$form = this.$node.closest('form');
+
       this.on('click touch', function(e) {
         e.preventDefault();
-        this.$node.html(loader).siblings('input').css('opacity', 0.6);
+        this.buttonText = this.$node.text();
+        this.$node.html(loader);
+        this.siblings.css('opacity', 0.6);
+        var data = this.$form.serializeArray();
+        console.log(data);
+      });
+
+      this.on('uiFormProcessed', function() {
+        this.siblings.css('opacity', 1);
+        this.$node.html(this.buttonText);
       });
     });
   }
