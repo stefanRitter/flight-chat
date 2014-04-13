794eec5946f834ff163d519420f6bc3dd5594d97
stefanRitter <stefan@stefanritter.com>
2014-04-13 12:17:08 +0100

    test form_submit

--- test/spec/component_ui/form_submit.spec.js
+++ test/spec/component_ui/form_submit.spec.js
@@ -2,22 +2,40 @@
 
 describeComponent('component_ui/form_submit', function () {
 
-  // Initialize the component and attach it to the DOM
   beforeEach(function () {
-    setupComponent();
+    setupComponent(readFixtures('form.html'));
   });
 
-  it('should be defined', function () {
-    expect(this.component).toBeDefined();
+  it('should trigger the event specified in the data-event attribute on submit', function () {
+    spyOnEvent(document, 'uiCustomFormSubmitEvent');
+    $('button[type=submit]').trigger('click');
+    expect('uiCustomFormSubmitEvent').toHaveBeenTriggeredOn(document);
   });
 
-  it('should do something');
+  it('should insert errors on uiFormError', function() {
+    $('button[type=submit]').trigger('click');
+    this.component.trigger('uiFormError', {error: 'error text'});
+    expect($('.error')).toHaveText('error text');
+  });
 
+  it('should show form is being processed on submit', function() {
+    $('button[type=submit]').trigger('click');
+    expect($('button[type=submit]'))
+      .toHaveHtml('<img class="animRotateRound" src="img/spinner-white.png">');
+  });
+
+  it('should show form is available again on uiFormProcessed', function() {
+    var originalButtonHtml = $('button[type=submit]').html();
+    $('button[type=submit]').trigger('click');
+    this.component.trigger('uiFormProcessed');
+    expect($('button[type=submit]')).toHaveHtml(originalButtonHtml);
+  });
+
+  it('should reset the form on uiFormReset', function() {
+    $('button[type=submit]').trigger('click');
+    
+    $('input[type="text"]').val('test value');
+    this.component.trigger('uiFormReset');
+    expect($('input[type="text"]')).toHaveValue('');
+  });
 });
-
-/*
-this.on('click touch', { buttonSelector: this.submit });
-this.on('uiFormProcessed', this.reactivateForm);
-this.on('uiFormError', this.processFormErrors);
-this.on('uiFormReset', this.processFormReset);
-*/
