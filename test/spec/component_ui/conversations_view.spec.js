c1febdd66b90542c2b3e38c26fe84e4e82799c98
stefanRitter <stefan@stefanritter.com>
2014-04-13 14:44:52 +0100

    add spec

--- test/spec/component_ui/conversations_view.spec.js
+++ test/spec/component_ui/conversations_view.spec.js
@@ -1,16 +1,32 @@
 'use strict';
 
 describeComponent('component_ui/conversations_view', function () {
+  var conversationData = {
+    conversationId: 5,
+    user: {
+      imageUrl: '',
+      name: 'test user'
+    },
+    text: 'test message'
+  };
 
-  // Initialize the component and attach it to the DOM
   beforeEach(function () {
-    setupComponent();
+    setupComponent(readFixtures('conversations_view.html'));
+    this.component.trigger('dataNewConversation', conversationData);
   });
 
-  it('should be defined', function () {
-    expect(this.component).toBeDefined();
+  it('should add a new conversation on dataNewConversation', function () {
+    expect($('#chats a').length).toEqual(1);
   });
 
-  it('should do something');
+  it('should mark a new conversation as seen on uiConversationSeen', function () {
+    this.component.trigger('uiConversationSeen', conversationData);
+    expect($('a.chat-link')).not.toHaveClass('active');
+  });
 
+  it('should mark a conversation as unread on dataMessageReceived', function () {
+    this.component.trigger('uiConversationSeen', conversationData);
+    this.component.trigger('dataMessageReceived', conversationData);
+    expect($('a.chat-link')).toHaveClass('active');
+  });
 });
