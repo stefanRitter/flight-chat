fb1fbec2c6cfd52af9b151f2176a73a9114a5863
stefanRitter <stefan@stefanritter.com>
2014-04-13 10:37:00 +0100

    add missing spec

---
+++ test/spec/component_ui/conversations_view.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describeComponent('component_ui/conversations_view', function () {
+
+  // Initialize the component and attach it to the DOM
+  beforeEach(function () {
+    setupComponent();
+  });
+
+  it('should be defined', function () {
+    expect(this.component).toBeDefined();
+  });
+
+  it('should do something');
+
+});
---
+++ test/spec/component_ui/form_submit.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describeComponent('component_ui/form_submit', function () {
+
+  // Initialize the component and attach it to the DOM
+  beforeEach(function () {
+    setupComponent();
+  });
+
+  it('should be defined', function () {
+    expect(this.component).toBeDefined();
+  });
+
+  it('should do something');
+
+});
